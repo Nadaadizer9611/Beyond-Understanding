8ac5a8aa76475ffd44a6bc2c4714d98a84d5cda5
Nadaadizer9611 <67803943+Nadaadizer9611@users.noreply.github.com>
2024-01-24 23:20:58 -0300

    Add files via upload

---
+++ content/VernasTechTree.java
@@ -0,0 +1,32 @@
+package BV.content;
+
+import mindustry.content.*;
+import mindustry.game.Objectives.*;
+
+import static arc.struct.Seq.*;
+import static mindustry.content.TechTree.*;
+import static bv.content.BVBlocks.*;
+import static bv.content.BVItems.*;
+import static bv.content.BVLiquids.*;
+import static bv.content.BVSectorPresets.*;
+import static bv.content.BVUnitTypes.*;
+import static bv.content.blocks.BVForce.*;
+import static bv.content.blocks.BVVibration.*;
+
+public class BVTechTree {
+  public static TechNode root;
+
+  public static void load() {
+    root = nodeRoot("Vernas", exploration-core, () -> {
+      // region items
+      nodeProduce(aluminum, () -> {
+        nodeProduce(steam, () -> nodeProduce(fungi, () -> {}));
+        nodeProduce(lithium, () -> nodeProduce(platinum, () -> nodeProduce(cobalt, () -> {})));
+        nodeProduce(cooper, () -> nodeProduce(titanium, () -> {}));
+      });
+      // endregion
+
+    });
+    BVPlanets.mangle.techTree = root;
+  }
+}
